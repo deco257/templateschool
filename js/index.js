c0e6eac496682e085ca97ef63b0502f330e307ec
deco257 <derickkazikazi@gmail.com>
2023-01-02 12:41:02 +0200

    initial comit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+function refresh(){
+    var t = 1000; // rafraîchissement en millisecondes
+    setTimeout('showDate()',t)
+}
+
+function showDate() {
+    var date = new Date()
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    if( h < 10 ){ h = '0' + h; }
+    if( m < 10 ){ m = '0' + m; }
+    if( s < 10 ){ s = '0' + s; }
+    var time = h + ':' + m + ':' + s
+    document.getElementById('horloge').innerHTML = time;
+    refresh();
+ }
+
+
+
+$('document').ready(function(){
+    $(' ul #list').hide();
+
+ $('.men1').mouseenter(function(){
+
+    $(' ul #list').show().css({'list-style':'none',
+    'background':'white','color':'black'});
+
+});
+$('.men1').mouseleave(function(){
+
+$('ul #list').hide();
+});
+
+})
